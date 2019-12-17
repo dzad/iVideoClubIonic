0c9e3be8a6fc7b5a92c21731d2b777016c26a6b8
MeZayd <zm10@alu.ua.es>
2019-12-17 20:08:06 +0100

    Página listado de películas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'curriculum', pathMatch: 'full' },
+  { path: '', redirectTo: 'videoclub', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'autor',
