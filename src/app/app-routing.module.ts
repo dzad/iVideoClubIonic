9771976014904221e9ad7dc5540750379a837ae4
MeZayd <zm10@alu.ua.es>
2019-12-17 19:54:49 +0100

    Aspecto de la página autor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'autor', pathMatch: 'full' },
+  { path: '', redirectTo: 'curriculum', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
     path: 'autor',
