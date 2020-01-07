c9176580078ef5ca6cf55d74a4516e84f8fc4632
MeZayd <zm10@alu.ua.es>
2020-01-07 19:09:48 +0100

    Nueva página detalle

---
+++ src/app/detalle/detalle.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle',
+  templateUrl: './detalle.page.html',
+  styleUrls: ['./detalle.page.scss'],
+})
+export class DetallePage implements OnInit {
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(){
+  }
+
+}
