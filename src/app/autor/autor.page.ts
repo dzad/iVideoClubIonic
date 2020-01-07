b7664cf6f593e11b75fa90391f1ebd0ecca9357d
MeZayd <zm10@alu.ua.es>
2020-01-07 19:42:29 +0100

    Página de autor y currículum

--- src/app/autor/autor.page.ts
+++ src/app/autor/autor.page.ts
@@ -8,7 +8,22 @@ import { Router } from '@angular/router';
 })
 export class AutorPage implements OnInit, OnDestroy {
 
-  constructor(private router: Router) {}
+  autor: {
+    name: string,
+    email: string,
+    twitter: string,
+    phone: string
+  };
+  mailto:string;
+  twitterProfile:string;
+  telTo:string;
+
+  constructor(private router: Router) {
+    this.autor = {name: "ZAYD", email: "mekhaznizayd@gmail.com", twitter: "mekhaznizayd", phone: "698556331"};
+    this.mailto = "mailto:"+this.autor.email;
+    this.twitterProfile = "https://twitter.com/"+this.autor.twitter;
+    this.telTo = "tel:"+this.autor.phone;
+  }
 
   ngOnInit() {
     console.log('ngOnInit Autor');
--- src/app/curriculum/curriculum.page.ts
+++ src/app/curriculum/curriculum.page.ts
@@ -6,8 +6,13 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./curriculum.page.scss'],
 })
 export class CurriculumPage implements OnInit, OnDestroy {
+  listaTrabajos: {fecha: string, desc: string}[];
 
-  constructor() { }
+  constructor() {
+    this.listaTrabajos = [{fecha:"2010-actualidad",desc:"Profesor del Máster Universitario en Desarrollo de Software para Dispositvos móviles."},
+                          {fecha:"2015-actualidad",desc:"Professor del Máster Universitario en Desarrollo de Aplicaciones y Servicios Web"},
+                          {fecha:"2008-2010", desc:"Sexador de pollos."}]
+  }
 
   ngOnInit() {
     console.log('ngOnInit Curriculum');
